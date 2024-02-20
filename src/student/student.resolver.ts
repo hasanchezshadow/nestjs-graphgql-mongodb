8939219fbbc5d14774e6586d74d4fbb5550e386e
Hernan.Sanchez <hernan.sanchez@musala.com>
2024-02-20 12:03:56 +0200

    Adding create student mutation. Adding get all students query. Integrating resources related to those functionalities

---
+++ src/student/student.resolver.ts
@@ -0,0 +1,21 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { StudentType } from './student.type';
+import { StudentService } from './student.service';
+import { CreateStudentInput } from './create-student.input';
+
+@Resolver(() => StudentType)
+export class StudentResolver {
+  constructor(private studentService: StudentService) {}
+
+  @Mutation(() => StudentType)
+  createStudent(
+    @Args('createStudentInput') createStudentInput: CreateStudentInput,
+  ) {
+    return this.studentService.createStudent(createStudentInput);
+  }
+
+  @Query(() => [StudentType])
+  students() {
+    return this.studentService.getStudents();
+  }
+}
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Student } from './student.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateStudentInput } from './create-student.input';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(
+    @InjectRepository(Student) private studentRespository: Repository<Student>,
+  ) {}
+
+  createStudent(createStudentInput: CreateStudentInput): Promise<Student> {
+    const student = this.studentRespository.create({
+      id: uuid(),
+      ...createStudentInput,
+    });
+    return this.studentRespository.save(student);
+  }
+
+  getStudents(): Promise<Student[]> {
+    return this.studentRespository.find({});
+  }
+}
